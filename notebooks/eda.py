5c7df953b643a6b273b183b45292de59a9f62f8d
Zach Laguna <zachlaguna@gmail.com>
2019-04-17 16:47:04 -0400

    added jupyter_notebook_config.py

---
+++ notebooks/eda.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Explore & Clean the Data
+
+# In[1]:
+
+
+import pandas as pd
+from IPython.display import display
+
+
+# In[2]:
+
+
+df = pd.read_csv('../data/raw/pbp-2018.csv')
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+display(df.head())
+
+
+# In[ ]:
+
+
+
+
